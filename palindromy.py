45ff2333452c23933d78dc30cad68747fbe80190
Jagoda Giurg <jagoda.giurg@gmail.com>
2022-10-02 15:00:29 +0100

    Simplify if statement and add test

--- palindromy.py
+++ palindromy.py
@@ -1,6 +1,6 @@
 def palindrome_check(word):
     reversed_word = word[::-1]
-    if reversed_word == word:
-        return True
-    else:
-        return False
+    return reversed_word == word
+
+print(palindrome_check('kajak'))
+print(palindrome_check('kajaki'))
